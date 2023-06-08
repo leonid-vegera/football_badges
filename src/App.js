55676f4d262e5ae83324d41c16f25f9937b09d10
Leonid Vegera <leonid.vegera@gmail.com>
2023-06-08 19:43:47 +0300

    Initial еуьзÐtemplate

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const Timer = () => {
+  const [clock, setClock] = useState(new Date());
+
+  useEffect(() => {
+    setTimeout(() => {
+      setClock(new Date())
+    }, 1000)
+
+  }, [clock]);
+
+  return (
+    <div>
+      <h1>Час фортуни:</h1>
+      <h2>{clock.toLocaleTimeString()}</h2>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <Timer/>
       </header>
     </div>
   );
